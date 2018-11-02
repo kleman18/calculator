eef115a1d50ff475cd724146b44c4e7d7bff95c4
Volodymyr Kleman <v.kleman1996@gmail.com>
2018-11-02 13:22:30 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -8,9 +8,48 @@ function digitOperClick(e) {
 }
 
 document.querySelector('.equal')
-.addEventListener('click', equal);
+    .addEventListener('click', equal);
 
 function equal() {
     const display = document.querySelector('.display');
     display.value = eval(display.value);
+}
+
+document.querySelector('.sqrt')
+    .addEventListener('click', sqrt);
+
+function sqrt() {
+    const display = document.querySelector('.display');
+    display.value = Math.sqrt(display.value);
+}
+
+document.querySelector('.sqr')
+    .addEventListener('click', sqr);
+
+function sqr() {
+    const display = document.querySelector('.display');
+    display.value *= (display.value);
+}
+
+document.querySelector('.factorial')
+    .addEventListener('click', factorial);
+
+function factorial() {
+    const display = document.querySelector('.display');
+    if (display.value >= 1 || display.value % 1 === 0) {
+        let n = 1;
+        for (let i = 1; i <= display.value; i++) {
+            n = n * i;
+        }
+        display.value = n;
+    }
+    else display.value = "error";
+}
+
+document.querySelector('.clear')
+    .addEventListener('click', clear);
+
+function clear() {
+    const display = document.querySelector('.display');
+    display.value = '';
 }
